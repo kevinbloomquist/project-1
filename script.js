e9c14b31f91e342ebecbfb75643d0a019a33f8e2
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-16 16:39:12 -0700

    useArray and extendArray are working wel together and functioning as expected

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ console.log(randomNum);
 
 
 // generate arrays for use in the functionality of game stages
-var callArray = [1,2,1];
+var callArray = [];
 console.log("callArray "+callArray);
 
 var useArray = [];
@@ -21,7 +21,7 @@ console.log("useArray " + useArray);
 var extendArray = function(callArray) {
 	
 console.log(callArray);
-
+var randomNum = Math.floor(Math.random()*(3-1) +1);
 callArray.push(randomNum);
 useArray = callArray;
 
@@ -32,4 +32,20 @@ console.log("useArray " + useArray);
 
 extendArray(callArray);
 console.log("UseArray "+useArray);
+
+// create loop to manage display CSS vaues
+
+var funcLoop = function(useArray) {
+	for (i=0; i<useArray.length; i++) {
+	if (UseArray[i]%2 ===0) {
+		console.log("BOX 2 STUFF!!!");
+	} else if (useArray[i]%2===1) {
+		console.log("BOX 1 STUFF!!!");
+	}
+}
+};
+
+
+
+
 
