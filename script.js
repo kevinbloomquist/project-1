4536f3d9a199f7bafebf90b163eb554da15db720
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-16 16:42:07 -0700

    funcLoop working as expected

--- script.js
+++ script.js
@@ -37,7 +37,8 @@ console.log("UseArray "+useArray);
 
 var funcLoop = function(useArray) {
 	for (i=0; i<useArray.length; i++) {
-	if (UseArray[i]%2 ===0) {
+
+	if (useArray[i]%2 ===0) {
 		console.log("BOX 2 STUFF!!!");
 	} else if (useArray[i]%2===1) {
 		console.log("BOX 1 STUFF!!!");
