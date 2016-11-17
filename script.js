e7671cf4413215468ffaa689b2176698345cc74d
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-17 12:22:18 -0700

    safety commit befor I dig into timing issues

--- script.js
+++ script.js
@@ -44,7 +44,8 @@ console.log("UseArray "+useArray);
 
 // create loop to manage display CSS vaues
 
-
+// turnon
+// turn off
 
 var funcLoop = function(useArray) {
 	for (i=0; i<useArray.length; i++) {
@@ -58,25 +59,37 @@ var funcLoop = function(useArray) {
 	}*/
 // *************************************************************remove above
 // pick-up here: added setTimeout, 3000 to working document function to test. turn2on works but turn2off doesn't and delay coordination is off
-		setTimeout(turn2on,3000);
+		setTimeout(turn2on,2000);
 
 		function turn2on() {
 		document.getElementsByClassName("box2")[0].setAttribute("id","on");
 	}
 		// wait...
-		setTimeout(turn2off,3000);
+		setTimeout(turn2off,4000);
 
 		function turn2off() {
-		document.getElementsByClassName("box2")[0].setAttribute("id","test");
+		document.getElementsByClassName("box2")[0].removeAttribute("id");
 			console.log("turn2off fired!");
 	}
 		// wait...
 		console.log("BOX 2 STUFF!!!");
 	} else if (useArray[i]%2===1) {
+
+		setTimeout(turn1on,1000);
+
+		function turn1on() {
 		document.getElementsByClassName("box1")[0].setAttribute("id","on");
-		document.getElementsByClassName("box2")[0].removeAttribute("id");
+
+	}
+		// wait...
+		setTimeout(turn1off,3000);
+
+		function turn1off() {
+		document.getElementsByClassName("box1")[0].removeAttribute("id");
+	}
 		console.log("BOX 1 STUFF!!!");
 	}
+		// wait...
 }
 };
 
