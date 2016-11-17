2b350f968dc641c12e5993cdd208474bc1734f34
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-16 19:46:29 -0700

    safety

--- script.js
+++ script.js
@@ -43,23 +43,13 @@ extendArray(callArray);
 console.log("UseArray "+useArray);
 
 // create loop to manage display CSS vaues
-// test setTimeout
-// ******************************************************************remove below
-setTimeout(myFunction,3000);
-
-function myFunction() {
-    alert('Hello');
-}
-// ****************************************************************remove above
-
-
 
 
 
 var funcLoop = function(useArray) {
 	for (i=0; i<useArray.length; i++) {
-// ************************************************************remove below
 	if (useArray[i]%2 ===0) {
+// ************************************************************remove below
 
 		/*setTimeout(myFunction,3000);
 
@@ -67,22 +57,23 @@ var funcLoop = function(useArray) {
     	alert('Hello');
 	}*/
 // *************************************************************remove above
-// pick-up here: added setTimeout, 3000 to working document function to test. turn2on works but turn2off doesn't
+// pick-up here: added setTimeout, 3000 to working document function to test. turn2on works but turn2off doesn't and delay coordination is off
 		setTimeout(turn2on,3000);
 
-		function turn2on() 
-		{document.getElementsByClassName("box2")[0].setAttribute("id","on");
+		function turn2on() {
+		document.getElementsByClassName("box2")[0].setAttribute("id","on");
 	}
 		// wait...
-		setTimeout(turn2on,3000);
+		setTimeout(turn2off,3000);
 
 		function turn2off() {
-		document.getElementsByClassName("box2")[0].removeAttribute("id");
+		document.getElementsByClassName("box2")[0].setAttribute("id","test");
+			console.log("turn2off fired!");
 	}
 		// wait...
 		console.log("BOX 2 STUFF!!!");
 	} else if (useArray[i]%2===1) {
-		setTimeout(document.getElementsByClassName("box1")[0].setAttribute("id","on"),3000);
+		document.getElementsByClassName("box1")[0].setAttribute("id","on");
 		document.getElementsByClassName("box2")[0].removeAttribute("id");
 		console.log("BOX 1 STUFF!!!");
 	}
