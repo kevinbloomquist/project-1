19bb5599995f848a710533dc29ffa14902d7b45f
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-20 19:48:53 -0700

    worked around shift problem by adding a starting value to initial arrays. working on while loop for checkWin().

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ console.log("random number " +randomNum);
 
 
 // generate arrays for use in the functionality of game stages
-var callArray = [];
+var callArray = [1];
 console.log("callArray "+ callArray);
 
-var useArray = [];
+var useArray = [1];
 console.log("useArray " + useArray);
+
 
 var compareArray = [];
 console.log("compareArray "+ compareArray);
@@ -27,6 +28,7 @@ console.log(grabBox1);
 
 var grabBox2 = document.getElementsByClassName("box2");
 console.log(grabBox2); 
+
 
 
 // Function to extend the current random array by 1 new random number 1-4) -> saves to useArray
@@ -46,13 +48,13 @@ console.log("useArray " + useArray);
 extendArray(callArray);
 console.log("UseArray "+useArray);
 
+
+
+//***************************************start switch/ case approach here:
 // create functions to manage display CSS vaues
 
 // turnon...
 // turn off...
-
-//***************************************start switch/ case approach here
-
 function turn1on() {
 		document.getElementsByClassName("box1")[0].setAttribute("id","on");
 
@@ -131,87 +133,15 @@ var funcLoop = function(useArray) {
 };
 
 
-// pick-up here: added setTimeout, on and off work. offset timers to better understand...delay coordination is off trying switch context*/
-
-/*********************************************************************************Working for loop quarantine below
-var funcLoop = function(useArray) {
-	for (i=0; i<useArray.length; i++) {
-	if (useArray[i] ===2) {
-		setTimeout(turn2on,2000);
-
-		function turn2on() {
-		document.getElementsByClassName("box2")[0].setAttribute("id","on");
-	}
-		// wait...
-		setTimeout(turn2off,4000);
-
-		function turn2off() {
-		document.getElementsByClassName("box2")[0].removeAttribute("id");
-			console.log("turn2off fired!");
-	}
-		// wait...
-		console.log("BOX 2 STUFF!!!");
-	} else if (useArray[i]===1) {
-
-		setTimeout(turn1on,1000);
-
-		function turn1on() {
-		document.getElementsByClassName("box1")[0].setAttribute("id","on");
-
-	}
-		// wait...
-		setTimeout(turn1off,3000);
-
-		function turn1off() {
-		document.getElementsByClassName("box1")[0].removeAttribute("id");
-	}
-		console.log("BOX 1 STUFF!!!");
-
-	} else if (useArray[i]===3) {
-
-		setTimeout(turn3on,1000);
-
-		function turn3on() {
-		document.getElementsByClassName("box3")[0].setAttribute("id","on");
-
-	}
-		// wait...
-		setTimeout(turn3off,3000);
-
-		function turn3off() {
-		document.getElementsByClassName("box3")[0].removeAttribute("id");
-	}
-		console.log("BOX 3 STUFF!!!");
-		// wait...
-
-	} else if (useArray[i]===4) {
-
-		setTimeout(turn4on,2000);
-
-		function turn4on() {
-		document.getElementsByClassName("box4")[0].setAttribute("id","on");
-
-	}
-		// wait...
-		setTimeout(turn4off,4000);
-
-		function turn4off() {
-		document.getElementsByClassName("box4")[0].removeAttribute("id");
-	}
-		console.log("BOX 4 STUFF!!!");
-}
-}
-};
-*********************************************************************working for loop quarantine above*/
 
 // addEventListener to divs****************************************************************************
-var responseArray = [];
+var responseArray = [1];
 // diagnostic function
 var mark = function(b) {
 	console.log("clicked" + b);
 };
 
-// need to make visual response to user click
+//visual response to user click
 document.getElementsByClassName("box1")[0].addEventListener("click",function(){
 	responseArray.push(1);
 	mark(responseArray);
@@ -251,19 +181,25 @@ console.log(responseArray);
 var compare = _.isEqual(responseArray,shiftDecoy);
 console.log("compare is "+ compare);
 // dummy variable so useArray stays in tact across rounds.
+
 var shiftDecoy = useArray;
 
+// going to attempt to attach shiftBridge call to landing page button
+
+while (responseArray.length == shiftDecoy.length && responseArray.length>1) {
+	console.log("check!");
+}
+// *******************************************working above on solution to recognize check conditions->to 192 quarantine
 var checkWin = function() {
 // jettison shiftDecoy[0] before comparing to responseArray
-shiftDecoy.shift(); //need a solution (maybe something in callArr/useArray cycle) for this you need to pop for comparison inside this function but outside useArray has to remain intact.
 
 var compare = _.isEqual(responseArray,shiftDecoy);
-if (compare ===true) {
-// Alert win/lose----> change to modal
-alert("correct!");
-} else {
-	alert("beat it nerd!");
-}
+	if (compare ===true) {
+		// Alert win/lose----> change to modal
+		alert("correct!");
+	}else {
+		alert("beat it nerd!");
+	}
 };
 
 // variable and advance function for round display
@@ -276,18 +212,20 @@ roundNum++;
 // go will run extendArray run funcLoop and change round.
 // transition to next round: clear responseArray/ run extendArray/ run funcLoop and change round number
 document.getElementsByClassName("start")[0].addEventListener("click",function(){
-responseArray = [];
-extendArray(useArray);
-funcLoop(useArray);
-roundAdvance();
-colorFunc();
-document.getElementsByClassName("roundBox")[0].innerHTML="ROUND " + roundNum;
-	});
+	responseArray = [1];
+	extendArray(useArray);
+	funcLoop(useArray);
+	roundAdvance();
+	colorFunc();
+	document.getElementsByClassName("roundBox")[0].innerHTML="ROUND " + roundNum;
+
+});
 
 
 // when response.length = shiftDecoy.length -> run check win
 // ->trigger modal response (user closes) ten clicks go! (change alerts)
 // Scoring metric: background color -> each round passes will add to rgba values
+// stretch-> the color of each box slowly transitions to it's compliment for added difficulty
 var n = 265;
 // made variables for easy expansion of functionality later
 var redVal = 255;
@@ -299,4 +237,5 @@ var colorFunc = function() {
 	document.body.style.backgroundColor = "rgba("+n+"," +n+","+n+", 0.8)";
 
 };
+
 
